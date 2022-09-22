980dbd94189783df6ee149a26399dcd121603a42
jonatas-tavares <jontavpess@gmail.com>
2022-09-22 20:13:37 -0300

    Prpreparation to merge in main

--- build_spark.sh
+++ build_spark.sh
@@ -1,1 +1,1 @@
-docker build  -f ./jupyter-spark/Dockerfile_dev -t jontavpess/jupyter-spark-base ./jupyter-spark
+docker build  -f ./jupyter-spark/Dockerfile -t jontavpess/jupyter-spark-base ./jupyter-spark
