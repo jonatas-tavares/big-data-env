941cf33edbe6e91c1f66ce16306174625e555c1e
jonatas-tavares <jontavpess@gmail.com>
2022-09-22 21:22:00 -0300

    Reestruturation of dir hierarchy

--- build_spark.sh
+++ build_spark.sh
@@ -1,1 +1,1 @@
-docker build  -f ./jupyter-spark/Dockerfile_dev -t jontavpess/jupyter-spark-base ./jupyter-spark
+docker build  -f ./docker-images/jupyter-spark/Dockerfile_dev -t jontavpess/jupyter-spark-base ./jupyter-spark
