575d9405a04ff1686cb1e93eea3cbd13ec5f957b
jonatas-tavares <jontavpess@gmail.com>
2022-09-24 14:58:45 -0300

    Modified jupyter launch dir

--- docker-images/jupyter-spark/bootstrap.sh
+++ docker-images/jupyter-spark/bootstrap.sh
@@ -11,6 +11,7 @@ hdfs dfs -put /usr/spark/jars/*.jar /user/spark/share/lib
 
 echo "-- Iniciar Notebook --"
 sleep 5
+cd /mnt/jupyter
 jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
 
 while :; do sleep 2073600; done
